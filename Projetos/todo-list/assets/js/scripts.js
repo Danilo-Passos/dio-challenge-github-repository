e9d074bb57d7e118a4f592f28804b94a44e890ab
Danilo Passos <daan_passos@hotmail.com>
2022-07-18 09:49:21 -0300

    Atualizando Projeto To-Do List

--- Projetos/todo-list/assets/js/scripts.js
+++ Projetos/todo-list/assets/js/scripts.js
@@ -10,21 +10,24 @@ form.onsubmit = function (e) {
 };
 
 function addTask(description) {
-	const taskContainer = document.createElement('div');
-	const newTask = document.createElement('input');
-	const taskLabel = document.createElement('label');
-	const taskDescriptionNode = document.createTextNode(description);
+	if (description === "") {
+		return;
+	}
+		const taskContainer = document.createElement('div');
+		const newTask = document.createElement('input');
+		const taskLabel = document.createElement('label');
+		const taskDescriptionNode = document.createTextNode(description);
+	
+		newTask.setAttribute('type', 'checkbox');
+		newTask.setAttribute('name', description);
+		newTask.setAttribute('id', description);
 
-	newTask.setAttribute('type', 'checkbox');
-	newTask.setAttribute('name', description);
-	newTask.setAttribute('id', description);
+		taskLabel.setAttribute('for', description);
+		taskLabel.appendChild(taskDescriptionNode);
 
-	taskLabel.setAttribute('for', description);
-	taskLabel.appendChild(taskDescriptionNode);
+		taskContainer.classList.add('task-item');
+		taskContainer.appendChild(newTask);
+		taskContainer.appendChild(taskLabel);
 
-	taskContainer.classList.add('task-item');
-	taskContainer.appendChild(newTask);
-	taskContainer.appendChild(taskLabel);
-
-	taskList.appendChild(taskContainer);
+		taskList.appendChild(taskContainer);
 }
